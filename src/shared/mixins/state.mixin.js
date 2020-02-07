ae846a6380d6b25cbc8a95956f40ed344c565881
Dhiego <dhiego_henrique20@yahoo.com.br>
2020-02-07 16:18:08 -0300

    add state.mixin

---
+++ src/shared/mixins/state.mixin.js
@@ -0,0 +1,37 @@
+const mixin = {
+  methods: {
+    getStateAcronyms () {
+      return [
+        'AC',
+        'AL',
+        'AP',
+        'AM',
+        'BA',
+        'CE',
+        'DF',
+        'ES',
+        'GO',
+        'MA',
+        'MT',
+        'MS',
+        'MG',
+        'PA',
+        'PB',
+        'PR',
+        'PE',
+        'PI',
+        'RJ',
+        'RN',
+        'RS',
+        'RO',
+        'RR',
+        'SC',
+        'SP',
+        'SE',
+        'TO'
+      ]
+    }
+  }
+}
+
+export default mixin
